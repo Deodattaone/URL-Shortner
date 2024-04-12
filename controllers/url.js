95fcbbd59cb8533b1c4b3a677ded557a130af2d3
Deodatta Newalkar <deodattaone@gmail.com>
2024-04-12 20:10:54 +0530

    Added a route to get analytics

--- controllers/url.js
+++ controllers/url.js
@@ -15,6 +15,15 @@ async function handleGenerateNewShortURL(req,res){
 
 }
 
+async function handleGetAnalytics(req,res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({
+        totalClicks : result.visitHistory.length,
+        analytics : result.visitHistory,
+    });
+}
+
 module.exports={
-    handleGenerateNewShortURL,
+    handleGenerateNewShortURL,handleGetAnalytics,
 }
--- routes/url.js
+++ routes/url.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { handleGenerateNewShortURL } = require('../controllers/url');
+const { handleGenerateNewShortURL,handleGetAnalytics } = require('../controllers/url');
 const router = express.Router();
+
+router.get('/analytics/:shortId',handleGetAnalytics);
+
+
 router.post('/',handleGenerateNewShortURL);
 module.exports = router;
